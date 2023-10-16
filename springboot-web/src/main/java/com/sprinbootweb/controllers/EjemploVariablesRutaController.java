9022b46cdfb9e29c01c9811d53063702c82b7ccc
Josué Rivera <keko.rai.jr@gmail.com>
2023-10-16 14:52:42 -0600

    Ejemplo de MVC completo

---
+++ springboot-web/src/main/java/com/sprinbootweb/controllers/EjemploVariablesRutaController.java
@@ -0,0 +1,41 @@
+package com.sprinbootweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/variables")
+public class EjemploVariablesRutaController {
+
+    @GetMapping({"/", "/index", "/home", ""})
+    public String index(Model model) {
+        model.addAttribute("titulo", "Recibir parametros de la ruta (@PathVariable)");
+
+        return "variables/index";
+    }
+
+    @GetMapping("/string/{texto}")
+    public String varibles(@PathVariable String texto, Model model) {
+        model.addAttribute("titulo", "Recibir parametros " +
+                "de la ruta (@PathVariable)");
+        model.addAttribute("resultado", "El texto enviado en " +
+                "la ruta es: " + texto);
+
+        return "variables/ver";
+    }
+
+    @GetMapping("/string/{texto}/{numero}")
+    public String variables(@PathVariable String texto, @PathVariable Integer numero,
+                            Model model) {
+        model.addAttribute("titulo", "Recibir parametros de la " +
+                "ruta (@PathVariable)");
+        model.addAttribute("resultado", "El texto enviado en la " +
+                "ruta es: " + texto +
+                " y el numero enviado es: " + numero);
+
+        return "variables/ver";
+    }
+}
--- springboot-web/src/main/java/com/sprinbootweb/controllers/IndexController.java
+++ springboot-web/src/main/java/com/sprinbootweb/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.sprinbootweb.controllers;
 
 import com.sprinbootweb.models.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,19 @@ import java.util.List;
 @RequestMapping("/app")
 public class IndexController {
 
+    @Value("${texto.indexcontroller.index.titulo}")
+    private String textoIndex;
+
+    @Value("${texto.indexcontroller.perfil.titulo}")
+    private String textoPerfil;
+
+    @Value("${texto.indexcontroller.listar.titulo}")
+    private String textoListar;
+
     @GetMapping({"/index", "/", "", "/home"})
     public String index(Model model) {
 
-        model.addAttribute("titulo", "Hola Spring Framework");
+        model.addAttribute("titulo", textoIndex);
 
         return "index";
     }
@@ -29,6 +39,7 @@ public class IndexController {
         usuario.setApellido("Rivera");
         usuario.setEmail("riverapj99@gmail.com");
 
+        model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
         model.addAttribute("usuario", usuario);
 
         return "perfil";
@@ -39,7 +50,7 @@ public class IndexController {
 
         List<Usuario> usuarios = poblarUsuario();
 
-        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("titulo", textoListar);
         model.addAttribute("usuarios", usuarios);
 
         return "listar";
